7447c684ab26fcb33540ce1476d69e92c4d1ec8c
Cristian Chiri <92327949+cris1198@users.noreply.github.com>
2026-05-28 14:36:42 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://cris1198.github.io'
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
